29c27e756cea9333657e6c40c54321cd58aa8244
SRIKANTH <aleti.srikanthreddy@gmail.com>
2024-08-28 20:57:54 -0500

    Adding another foreach loop inside

--- testscript.ps1
+++ testscript.ps1
@@ -6,15 +6,37 @@ foreach( $node in $inputids ){
 
     Write-Host $node
     Write-Host '---------------'
-
-    $obj = [PSCustomObject]@{ 
-        id = $node.ToString()
-        datasourcetype = "test"
-        datasourceId = "testId#"
+    
+    #this is just test object
+    $responseObj = [PSCustomObject]@{
+        datasourceId = 'test-'+$node.ToString()
+        datasourcetype = "sql-"+$node.ToString()
     }
+    
+    $responseObj2 = [PSCustomObject]@{
+        datasourceId = 'responseObj2 - '+$node.ToString()
+        datasourcetype = "responseObj2 - "+$node.ToString()
+    }
+    
+    #this is just test object List with above object
+    $responseList = $responseObj, $responseObj2
 
-    $finaldata += $obj
+    foreach($item in $responseList){
+
+        $obj = [PSCustomObject]@{ 
+            id = $node.ToString()
+            datasourcetype = $item.datasourceId
+            datasourceId = $item.datasourcetype
+        }
+    
+        $finaldata += $obj
+    }
+    
     $obj = @();
+    $responseObj = @();
+    $responseObj2 = @();
+
+    $responseList= @();
 }
 
-$finaldata | Export-Csv -Path "C:\Users\Downloads\data.csv"
+$finaldata | Export-Csv -Path "C:\Users\saleti\Downloads\data.csv"
